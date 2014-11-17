1abeca3c302edeb6581665aa1f47f267984c7d28
vfalduto <vincent.falduto@c4mprod.com>
2014-11-17 17:26:18 +0100

    Fixed deprecated scoped function replace by where(nil)

--- lib/rails/cache/extended.rb
+++ lib/rails/cache/extended.rb
@@ -3,7 +3,7 @@ require 'rails/cache/version'
   module ActiveRecord
     class Base
       def self.cache_key
-        Digest::MD5.hexdigest "#{scoped.maximum(:updated_at).try(:to_i)}-#{scoped.count}"
+        Digest::MD5.hexdigest "#{where(nil).maximum(:updated_at).try(:to_i)}-#{where(nil).count}"
         #scoped.select("md5(COALESCE(date_part('epoch', MAX(updated_at))::char, '') || '-' || COUNT(*)) AS collection_cache_key").first.collection_cache_key
       end
     end
